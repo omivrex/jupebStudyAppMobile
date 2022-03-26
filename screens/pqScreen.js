e94b240d0a904f8e556feb7ede66e06e81eb9f66
RexOmiv <omivrex@gmail.com>
2022-03-26 12:37:49 +0100

    made answer section a different reusable component and refactored code also removed correct answer text beign displayed in answer section

--- screens/pqScreen.js
+++ screens/pqScreen.js
@@ -20,7 +20,7 @@ import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-nativ
 import { usePreventScreenCapture } from 'expo-screen-capture'
 import styles from '../styles/master.js';
 import pageStyles from '../styles/pqScreenStyles.js';
-import colors from '../styles/colors';
+import AnswerComponent from '../components/Answer.component';
 
 let renderCollection = true
 let IS_ANS_CARD_DISPLAYED = false
@@ -153,70 +153,7 @@ export default function pqScreen({navigation}) {
     const [ansCard, setansCard] = useState()
     const showAns = data => {
       setansCard(
-        <View style={pageStyles.answerCardWrapper}>
-            <View style={pageStyles.answerCard}>
-                {data&&data.correctAnswer? 
-                    <>
-                        <Text style={pageStyles.correctAnswerComponent}>Correct Answer: {data?data.correctAnswer:''}</Text>
-                    </>
-                    :
-                    <></>
-                }
-                
-                <MathJax
-                    html={
-                        `   
-                            <body style="width: 100%; overflow-y: auto;">
-                                <style>
-                                    * {
-                                        -webkit-user-select: none;
-                                        -moz-user-select: none;
-                                        -ms-user-select: none;
-                                        user-select: none;
-                                    }
-                                </style>
-                                <div style="font-size: 1.3em;
-                                    font-family: Roboto, sans-serif, san Francisco;
-                                    width: 90%;
-                                    margin: auto;
-                                    min-height: 50rem;
-                                ">
-                                    ${data&&data.answer?data.answer.replace('max-width: 180px;', 'max-width: 90vw;'):''}
-                                </div>
-                                <div style="height: 50%"></div>
-                            </body>
-                        
-                        `
-                    }
-                    mathJaxOptions={{
-                        messageStyle: "none",
-                        extensions: ["tex2jax.js"],
-                        jax: ["input/TeX", "output/HTML-CSS"],
-                        tex2jax: {
-                            inlineMath: [
-                                ["$", "$"],
-                                ["\\(", "\\)"],
-                            ],
-                            displayMath: [
-                                ["$$", "$$"],
-                                ["\\[", "\\]"],
-                            ],
-                            processEscapes: true,
-                        },
-                        TeX: {
-                            extensions: [
-                                "AMSmath.js",
-                                "AMSsymbols.js",
-                                "noErrors.js",
-                                "noUndefined.js",
-                            ],
-                        },
-
-                    }}
-                    style={{width: '98%', flex:2, marginTop: '5%', left: '1%'}}
-                />
-            </View>
-        </View>
+        <AnswerComponent data={data} />
       )
       IS_ANS_CARD_DISPLAYED = true
     }
